48e49867fdc325ef5ee9324814f30c4b3bce8a5d
simon berner <bernsimo@users.noreply.github.com>
2017-09-14 11:31:20 +0300

    lisatud muutujad nin if lause

--- esimene.php
+++ esimene.php
@@ -1,11 +1,36 @@
-!DOCTYPE html>
+<?php
+    //muutujad
+	$myName = "Simon";
+	$myFamilyName = "Berner";
+	//hindan päeva os
+	$hourNow = date ("H");
+	$partOfDay = "";
+	if ($hourNow < 8){
+	$partOfDay = "varajane hommik";
+
+	}
+	if ($hourNow >= 8 and $hourNow < 16){
+		$partOfDay = "koolipäev";
+	}
+	if ($hourNow > 16){
+		$partOfDay = "vaba aeg";
+	}
+//echo $partOfday
+?>
+
+
+<DOCTYPE html>
 <html>
 <body>
 
 <h1>My First Heading</h1>
 <p>My first paragraph.</p>
-<?php
-echo "algas php õppimine.";
+<?php 
+echo "<algas php õppimine.>";
+echo "<p>Täna on ";
+echo date("d.m.Y") ,",kell oli lehe avamise hetkel" , date("H:i:s");
+echo ", hetkel on " .$partOfDay .".</p>";
+
 ?>
 </body>
 </html>
