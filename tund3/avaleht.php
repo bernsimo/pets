5ed692169b8c58beb743135403c9aa4d3e7a3181
simon berner <bernsimo@users.noreply.github.com>
2017-09-21 11:43:09 +0300

    Lisasin kiisumiisu pildid

---
+++ tund3/avaleht.php
@@ -0,0 +1,32 @@
+<?php
+	//muutujad
+	$myName = "simon";
+	$myFamilyName = "berner";
+	
+		
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+		simon berner programmeerib veebi
+	</title>
+</head>
+<body>
+	<h1><?php echo $myName ." " .$myFamilyName; ?>, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	
+
+
+<p>Meie õpime <a href="http://www.tlu.ee">Tallinna Ülikoolis</a>.</p>
+<p>Minu esimene php leht on <a href="../esimene.php" >siin</a>.</p>
+<p>minu sõbra meelise leht on <a href="../../../~lutsmeel/Veebiprogrammeerimine">siin<a/> </p>
+<p>Pilte näeb <a href="photo.php">siin</a> </p>
+<img src="../../../pics/aww.jpg" alt="Tallinna Ülikool">
+
+
+</body>
+
+</html>
